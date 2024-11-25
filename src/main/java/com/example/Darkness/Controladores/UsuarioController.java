7d0feffada7e4763ab5fd3f1bc37a2f531467d2c
AlexsandroVS <alexsandro.valeriano1@gmail.com>
2024-11-24 23:19:56 -0500

    detalles finales

--- src/main/java/com/example/Darkness/Controladores/UsuarioController.java
+++ src/main/java/com/example/Darkness/Controladores/UsuarioController.java
@@ -1,12 +1,9 @@
 package com.example.Darkness.Controladores;
 
-
 import com.example.Darkness.Modelos.Manga;
 import com.example.Darkness.Modelos.Usuario;
 import com.example.Darkness.Servicios.MangaService;
 import com.example.Darkness.Servicios.UsuarioService;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,12 +17,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.Optional;
 
-
 @Controller
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
-    private final MangaService mangaService ;
+    private final MangaService mangaService;
 
     public UsuarioController(UsuarioService usuarioService, MangaService mangaService) {
         this.usuarioService = usuarioService;
@@ -36,37 +32,34 @@ public class UsuarioController {
     public String home(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
+
         Usuario usuario = usuarioService.buscarPorUsername(username);
-
-        // Obtener la lista de mangas y agregarla al modelo
         List<Manga> mangas = mangaService.findAll();
-        model.addAttribute("mangas", mangas);
 
-        // Redirigir a la página de administración si es un administrador
-        if (usuario.getRole().equals("ADMIN")) {
+        model.addAttribute("mangas", mangas);
+        if ("ADMIN".equals(usuario.getRole())) {
             return "redirect:/admin/usuarios";
         }
 
-        return "home";  // Página de inicio con la lista de mangas
+        return "home";
     }
+
     @PostMapping("/register")
-    public String UsuarioRegisterP(Usuario usuario, Model model) {
-        System.out.println("Recibiendo datos: " + usuario.getUsername() + " " + usuario.getPassword());
+    public String usuarioRegisterP(Usuario usuario, Model model) {
         try {
             usuario.setRole("USER");
             usuarioService.registrarUsuario(usuario);
-            System.out.println("Usuario registrado exitosamente: " + usuario.getUsername());
             return "redirect:/login";
         } catch (Exception e) {
-            System.err.println("Error al registrar el usuario: " + e.getMessage());
-            model.addAttribute("error", "Error al registrar el usuario.");
+            model.addAttribute("error", "Error al registrar el usuario: " + e.getMessage());
             return "user-register";
         }
     }
+
     @GetMapping("/register")
     public String mostrarFormularioRegistro(Model model) {
         model.addAttribute("usuario", new Usuario());
-        return "user-register"; // Asegúrate de que esta vista exista
+        return "user-register";
     }
 
     @GetMapping("/manga/{id}")
@@ -76,15 +69,12 @@ public class UsuarioController {
             model.addAttribute("manga", optionalManga.get());
             return "user-manga";
         } else {
-            // Maneja el caso de manga no encontrado
             return "redirect:/error";
         }
     }
 
-
-
     @GetMapping("/adminRegister")
-    public String RegisterAdmin(Model model) {
+    public String mostrarFormularioRegistroAdmin(Model model) {
         model.addAttribute("usuario", new Usuario());
         return "admin-register";
     }
@@ -93,21 +83,19 @@ public class UsuarioController {
     public String adminRegisterP(Usuario usuario, Model model) {
         try {
             usuario.setRole("ADMIN");
-
             usuarioService.registrarUsuario(usuario);
-
             return "redirect:/login";
         } catch (Exception e) {
-            model.addAttribute("error", "Error al registrar el administrador.");
+            model.addAttribute("error", "Error al registrar el administrador: " + e.getMessage());
             return "admin-register";
         }
     }
-
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
+
     @GetMapping("/logout")
     public String logout() {
         return "redirect:/login";
@@ -115,9 +103,6 @@ public class UsuarioController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin/usuarios")
-    @PostFilter("filterObject.role == 'USER'")
-    @PostAuthorize("hasPermission(returnObject, 'VIEW')")
-
     public String obtenerUsuarios(@RequestParam(required = false) String roleFilter, Model model) {
         List<Usuario> usuarios;
         if (roleFilter != null && !roleFilter.isEmpty()) {
@@ -128,7 +113,4 @@ public class UsuarioController {
         model.addAttribute("usuarios", usuarios);
         return "dashboard";
     }
-
-
-
 }
